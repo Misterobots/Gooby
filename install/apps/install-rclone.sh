4c18c72ff6be1536179f7345b3c3cbff1176e37b
Misterobots <pause.not@gmail.com>
2020-12-01 07:00:43 -0500

    Update install-rclone.sh
    add second rclone mount

--- install/apps/install-rclone.sh
+++ install/apps/install-rclone.sh
@@ -10,7 +10,13 @@ if [ -s ${CONFIGVARS}/checkapp ]; then
 
 	echo "Here is a list of the root folders in your mount:"
 	echo
+	echo "Mount 1"
 	ls -1 ${MOUNTTO}
+	echo
+	echo
+	echo
+	ls -l ${MOUNTTO2}
+	echo
 	echo
 	echo "If you're getting an error here, please"
 	echo "check your Rclone config settings."
@@ -58,11 +64,19 @@ else
 		echo "What is your media folder in ${RCLONESERVICE}?"
 		read -e -p "Leave empty for root - not recommended! (ex: Media)  " -i "" RCLONEFOLDER
 		echo
+		read -r RCLONESERVICE2 < ${HOME}/.config/rclone/rclone.conf; RCLONESERVICE2=${RCLONESERVICE2:1:-1}
+		read -e -p "Confirm that this is what you named your mount  " -i "${RCLONESERVICE2}" RCLONESERVICE2
+		echo
+		echo "What is your media folder in ${RCLONESERVICE2}?"
+		read -e -p "Leave empty for root - not recommended! (ex: Media)  " -i "" RCLONEFOLDER2
+		echo
 
 		# Installing Services
 
 		RCLONESERVICE=${RCLONESERVICE#:}; echo ${RCLONESERVICE} > ${CONFIGVARS}/rcloneservice
 		RCLONEFOLDER=${RCLONEFOLDER%/}; RCLONEFOLDER=${RCLONEFOLDER#/}; echo ${RCLONEFOLDER} > ${CONFIGVARS}/rclonefolder
+		RCLONESERVICE2=${RCLONESERVICE2#:}; echo ${RCLONESERVICE2} > ${CONFIGVARS}/rcloneservice2
+		RCLONEFOLDER2=${RCLONEFOLDER2%/}; RCLONEFOLDER2=${RCLONEFOLDER2#/}; echo ${RCLONEFOLDER2} > ${CONFIGVARS}/rclonefolder2
 
 		sudo sed -i 's/^#user_allow_other/user_allow_other/g' /etc/fuse.conf
 
@@ -71,6 +85,8 @@ else
 		mkdir -p ${HOME}/logs ${HOME}/Downloads
 		sudo mkdir -p ${RCLONEMOUNT} ${MOUNTTO} ${UPLOADS} ${UNSYNCED}
 		sudo chown -R ${USER}:${USER} ${HOME} ${CONFIGVARS} ${CONFIGS}/Docker ${RCLONEMOUNT} ${MOUNTTO} ${UPLOADS} ${UNSYNCED}
+		sudo mkdir -p ${RCLONEMOUNT2} ${MOUNTTO2} ${UPLOADS2} ${UNSYNCED2}
+		sudo chown -R ${USER}:${USER} ${HOME} ${CONFIGVARS} ${CONFIGS}/Docker ${RCLONEMOUNT2} ${MOUNTTO2} ${UPLOADS2} ${UNSYNCED2}
 
 		cat ${HOME}/.config/rclone/rclone.conf | grep "Local" > /dev/null
 		if ! [[ ${?} -eq 0 ]]; then
@@ -83,16 +99,27 @@ else
 			sudo mv ${HOME}/Downloads ${UPLOADS}
 			sudo ln -s ${UPLOADS}/Downloads ${HOME}/Downloads
 		fi
+		
+		if [ ! -d ${UPLOADS2}/Downloads ]; then
+			sudo mv ${HOME}/Downloads ${UPLOADS2}
+			sudo ln -s ${UPLOADS2}/Downloads ${HOME}/Downloads
+		fi
 
 		sudo rsync -a /opt/Gooby/scripts/services/rclonefs* /etc/systemd/system/
 		sudo rsync -a /opt/Gooby/scripts/services/mergerfs* /etc/systemd/system/
+		sudo rsync -a /opt/Gooby/scripts/services/rclonefm* /etc/systemd/system/
+		sudo rsync -a /opt/Gooby/scripts/services/mergerfm* /etc/systemd/system/
 		sudo sed -i "s/GOOBYUSER/${USER}/g" /etc/systemd/system/rclonefs.service
 		sudo sed -i "s/GOOBYUSER/${USER}/g" /etc/systemd/system/mergerfs.service
+		sudo sed -i "s/GOOBYUSER/${USER}/g" /etc/systemd/system/rclonefm.service
+		sudo sed -i "s/GOOBYUSER/${USER}/g" /etc/systemd/system/mergerfm.service
 
-		sudo systemctl enable rclonefs.service mergerfs.service
+		sudo systemctl enable rclonefs.service mergerfs.service rclonefm.service mergerfm.service
 		sudo systemctl daemon-reload
 		sudo systemctl start rclonefs.service
-		sleep 10; sudo systemctl start mergerfs.service
+		sudo systemctl start mergerfs.service
+		sudo systemctl start rclonefm.service
+		sleep 10; sudo systemctl start mergerfm.service
 
 		# Create syncmount cron
 		crontab -l | grep 'syncmount.sh' || (crontab -l 2>/dev/null; echo "0,15,30,45 * * * * /opt/Gooby/scripts/cron/syncmount.sh > /dev/null 2>&1") | crontab -
